5c05ae613485a8e018f66bb25e2c985e19753140
Oskar Homburg <oskar@ohomburg.de>
2019-01-20 17:51:49 +0100

    Better 🅱️ script

--- public/js/B.js
+++ public/js/B.js
@@ -1,20 +1,28 @@
 (function (window, document, undefined) {
-    var bcounter = 0;
+    let b = 0;
 
     function B() {
-        bcounter++;
-        var walk = document.createTreeWalker(document.body, NodeFilter.SHOW_TEXT, null, false);
-        console.log("bfactor", bfactor);
-        var n;
-        while (n = walk.nextNode()) { BText(n); }
+        b++;
+        let walk = document.createTreeWalker(document.body, NodeFilter.SHOW_TEXT, null, false);
+        let n;
+        while (n = walk.nextNode()) {
+            BText(n);
+        }
     }
 
     function BText(node) {
-        var regex;
-        switch (bcounter) {
-        case 0: return;
-        case 1: regex = /(^|(?<= ))[bBcC]/g;
+        let regex;
+        switch (b) {
+            case 0:
+                return;
+            case 1:
+                regex = /\b[bB]/g;
+                break;
+            case 2:
+                regex = /[BDP8]/g;
+                break;
         }
+        node.textContent = node.textContent.replace(regex, "\ud83c\udd71");
     }
 
     document.addEventListener('paste', (ev) => {
